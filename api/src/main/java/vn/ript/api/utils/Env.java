4f76574a37e64d2a27a951854a4524a04d48b5ba
tungsfer512 <tungbv5122001@gmail.com>
2024-03-29 09:34:19 +0700

    [FIX] delete local lib

--- api/src/main/java/vn/ript/api/utils/Env.java
+++ api/src/main/java/vn/ript/api/utils/Env.java
@@ -7,9 +7,9 @@ public class Env {
     public static String OPENAPI_DEV_URL = "https://api.ript.vn";
     public static String OPENAPI_PROD_URL = "https://api.ript.vn";
 
-    public static String KEYCLOAK_SERVER_URL = "http://192.168.10.224:8890";
-    public static String KEYCLOAK_REALM = "kc-thaibinh";
-    public static String KEYCLOAK_CLIENT_ID = "ssadapter";
-    public static String KEYCLOAK_CLIENT_SECRET = "NIoqa80Ek9aOcmImq7KnEx4lPYT9qMcq";
+    public static String KEYCLOAK_SERVER_URL = "";
+    public static String KEYCLOAK_REALM = "";
+    public static String KEYCLOAK_CLIENT_ID = "";
+    public static String KEYCLOAK_CLIENT_SECRET = "";
 
 }
